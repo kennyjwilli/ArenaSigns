6ca07ae9b215bb530a240d459054d8bfb94c0ee1
Kenny Williams <kenny@vectorgaming.net>
2013-11-26 18:54:00 -0800

    ArenaSign only stores location. There was no need to store everything about the sign.

--- src/net/arcanerealm/arenasigns/framework/ArenaSign.java
+++ src/net/arcanerealm/arenasigns/framework/ArenaSign.java
@@ -8,16 +8,18 @@ import net.vectorgaming.varenas.framework.Arena;
 import org.apache.commons.lang3.text.WordUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.block.Sign;
 
 /**
  *
  * @author Kenny
  */
-public class ArenaSign extends SignWrapper
+public class ArenaSign
 {
     private final String arenaName;
     private final String map;
+    private final Location loc;
     
     /**
      *
@@ -27,46 +29,27 @@ public class ArenaSign extends SignWrapper
      */
     public ArenaSign(Sign sign, String map, String arena)
     {
-        super(sign);
+        this.loc = sign.getLocation();
         this.map = map;
         this.arenaName = arena;
-        updateSign();
     }
     
     /**
-     * Updates the sign to all the latest values from the arena
+     *
+     * @param location Location of the sign
+     * @param map Map the sign is based off
+     * @param arena Arena the sign is based off
      */
-    public final void updateSign()
+    public ArenaSign(Location location, String map, String arena)
     {
-        this.setLine(0, ChatColor.BOLD+ArenaManager.getArenaSettings(map).getType());
-        
-        this.setLine(2, arenaName.split("_")[1]);
-        if(!ArenaManager.isArenaRunning(arenaName))
-        {
-            this.setLine(1, ChatColor.RED+WordUtils.capitalize(map));
-            this.setLine(3, "");
-        }else
-        {
-            this.setLine(1, ChatColor.GREEN+WordUtils.capitalize(map));
-            Arena arena = ArenaManager.getArena(arenaName);
-            int playersInArena;
-            if(ArenaPlayerManager.getPlayersInArena(arenaName) == null)
-            {
-                playersInArena = 0;
-            }else
-            {
-                playersInArena = ArenaPlayerManager.getPlayersInArena(arenaName).size();
-            }
-            int maxPlayers = arena.getSettings().getMaxPlayers();
-            this.setLine(3, playersInArena+"/"+maxPlayers);
-        }
-        Bukkit.getScheduler().scheduleSyncDelayedTask(ArenaSignsAPI.getPlugin(), new Runnable()
-        {
-            public void run()
-            {
-                update(true);
-            }
-        }, 1L);
+        this.loc = location;
+        this.map = map;
+        this.arenaName = arena;
+    }
+    
+    public Location getLocation()
+    {
+        return loc;
     }
     
     /**
@@ -85,5 +68,43 @@ public class ArenaSign extends SignWrapper
     public String getMap()
     {
         return map;
+    }
+    
+    public void update()
+    {
+        loc.getChunk().load();
+        final Sign sign = (Sign) loc.getBlock().getState();
+        
+        sign.setLine(0, ChatColor.BOLD+ArenaManager.getArenaSettings(map).getType());
+        sign.setLine(2, arenaName.split("_")[1]);
+        
+        if(!ArenaManager.isArenaRunning(arenaName))
+        {
+            sign.setLine(1, ChatColor.RED+WordUtils.capitalize(map));
+            sign.setLine(3, "");
+        }else
+        {
+            sign.setLine(1, ChatColor.GREEN+WordUtils.capitalize(map));
+            Arena arena = ArenaManager.getArena(arenaName);
+            int playersInArena;
+            if(ArenaPlayerManager.getPlayersInArena(arenaName) == null)
+            {
+                playersInArena = 0;
+            }else
+            {
+                playersInArena = ArenaPlayerManager.getPlayersInArena(arenaName).size();
+            }
+            int maxPlayers = arena.getSettings().getMaxPlayers();
+            sign.setLine(3, playersInArena+"/"+maxPlayers);
+        }
+        
+        Bukkit.getScheduler().scheduleSyncDelayedTask(ArenaSignsAPI.getPlugin(), new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                sign.update(true);
+            }
+        }, 1L);
     }
 }
